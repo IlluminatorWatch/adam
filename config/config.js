ec93e87908d822ba54f1222c8abeadfa99a71d6a
IlluminatorWatch <adam@pluxemburg.com>
2020-04-05 22:55:07 +0200

    försök med stripe som ej verkar funka ännu

--- config/config.js
+++ config/config.js
@@ -4,4 +4,14 @@ const config = {
     mail: process.env.MAIL
 }
 
+// if (process.env.NODE_ENV !== 'production') require('dotenv').config()
+// // const dotenv = require("dotenv").config()
+// const config = {
+//     // databaseURL: "mongodb+srv://Adam:King10Kong@cluster0-mqceb.mongodb.net/testlogin",
+//     // mail: "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"
+//     mail: process.env.MAIL,
+//     // Rakibs nyckel till sendgrid vi fixar en egen sen
+//     databaseURL: process.env.DATABASE
+// }
+
 module.exports = config;
